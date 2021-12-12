304d08fd4e16669fc096f3c4218749baf0e0f73d
fabio.ortiz.mt@correo.usa.edu.co <nombre-usuario@correo.com>
2021-12-12 09:11:03 -0500

    nn

--- src/main/java/Reto2_web/Reto2WebApplication.java
+++ src/main/java/Reto2_web/Reto2WebApplication.java
@@ -11,14 +11,17 @@
 
 package Reto2_web;
 
+import Reto2_web.interfaces.InterfaceOrder;
 import Reto2_web.interfaces.InterfaceSupplements;
 import Reto2_web.interfaces.InterfaceUser;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/*
 @SpringBootApplication
 public class Reto2WebApplication implements CommandLineRunner{
     @Autowired
@@ -32,5 +35,32 @@ public class Reto2WebApplication implements CommandLineRunner{
     public void run(String... args) throws Exception {
         interfaceSupplements.deleteAll();
         interfaceUser.deleteAll();
+    }
+}
+
+*/
+
+@SpringBootApplication
+public class Reto2WebApplication implements CommandLineRunner {
+
+    @Autowired
+    private InterfaceSupplements interfaceSupplements;
+    @Autowired
+    private InterfaceUser interfaceUser;
+    @Autowired
+    private InterfaceOrder interfaceOrder ;
+
+    public static void main(String[] args) {
+       SpringApplication.run(Reto2WebApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+        SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        interfaceSupplements.deleteAll();
+        interfaceUser.deleteAll();
+        interfaceOrder.deleteAll(); 
     }
 }
